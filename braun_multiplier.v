0ea189117ab68964877e4109126e73474b40401f
Arunvaishnav7435 <arunvaishnav7435@gmail.com>
2025-04-05 20:48:01 +0530

    rtl and tb fixes.

--- braun_multiplier.v
+++ braun_multiplier.v
@@ -6,5 +6,6 @@ module braun_multiplier #(parameter n = 2)(
   assign p[0] = a[0] & b[0];
   assign p[1] = (a[0] & b[1]) ^ (a[1] & b[0]);
   assign p[2] = (a[1] & b[1]) ^ (a[0] & a[1] & b[0] & b[1]);
+  assign p[3] = (a[1] & b[1]) & (a[0] & a[1] & b[0] & b[1]);
 endmodule
 // This module implements a Braun multiplier, which is a type of parallel multiplier.
