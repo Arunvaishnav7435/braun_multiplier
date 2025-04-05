84e8db97d8cf80989f094893e6f54e3466b4709c
Arunvaishnav7435 <arunvaishnav7435@gmail.com>
2025-04-05 18:28:44 +0530

    fixed warnings

--- braun_multiplier.v
+++ braun_multiplier.v
@@ -1,10 +1,8 @@
-module braun_multiplier (
+module braun_multiplier #(parameter n = 2)(
     input [n-1:0] a,
     input [n-1:0] b,
     output [2*n-1:0] p
 );
-  parameter n = 2;
-
   assign p[0] = a[0] & b[0];
   assign p[1] = (a[0] & b[1]) ^ (a[1] & b[0]);
   assign p[2] = (a[1] & b[1]) ^ (a[0] & a[1] & b[0] & b[1]);
