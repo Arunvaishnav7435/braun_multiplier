7fefad1e125fe3d5af3d5abe667682fecbf302b4
Arunvaishnav7435 <arunvaishnav7435@gmail.com>
2025-04-05 21:12:17 +0530

    complete classical tb, synthesis and simulation image.

--- braun_multiplier_tb_top.sv
+++ braun_multiplier_tb_top.sv
@@ -8,37 +8,47 @@ module braun_multiplier_tb_top;
         // Test case 1: 0 * 0 = 0
         a = 2'b00; b = 2'b00;
         #10;
-        assert (p == 3'b000) else $fatal("Test case 1 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b000) else $fatal("Test case 1 failed: %b * %b = %b", a, b, p);
 
         // Test case 2: 0 * 1 = 0
         a = 2'b00; b = 2'b01;
         #10;
-        assert (p == 3'b000) else $fatal("Test case 2 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b0000) else $fatal("Test case 2 failed: %b * %b = %b", a, b, p);
 
         // Test case 3: 1 * 0 = 0
         a = 2'b01; b = 2'b00;
         #10;
-        assert (p == 3'b000) else $fatal("Test case 3 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b0000) else $fatal("Test case 3 failed: %b * %b = %b", a, b, p);
 
         // Test case 4: 1 * 1 = 1
         a = 2'b01; b = 2'b01;
         #10;
-        assert (p == 3'b001) else $fatal("Test case 4 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b0001) else $fatal("Test case 4 failed: %b * %b = %b", a, b, p);
 
         // Test case 5: 1 * 2 = 2
         a = 2'b01; b = 2'b10;
         #10;
-        assert (p == 3'b010) else $fatal("Test case 5 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b0010) else $fatal("Test case 5 failed: %b * %b = %b", a, b, p);
 
         // Test case 6: 2 * 1 = 2
         a = 2'b10; b = 2'b01;
         #10;
-        assert (p == 3'b010) else $fatal("Test case 6 failed: %b * %b = %b", a, b, p);
+        assert (p == 'b0010) else $fatal("Test case 6 failed: %b * %b = %b", a, b, p);
+
+        // Test case 7: 2 * 2 = 4
+        a = 2'b10; b = 2'b10;
+        #10;
+        assert (p == 'b0100) else $fatal("Test case 7 failed: %b * %b = %b", a, b, p);
+        
+        // Test case 8: 2 * 3 = 6
+        a = 2'b10; b = 2'b11;
+        #10;
+        assert (p == 'b0110) else $fatal("Test case 8 failed: %b * %b = %b", a, b, p);
 
         // Test case 7: Max value test: (3*3=9)
-        a = 'h11; b = 'h11;
+        a = 2'b11; b = 2'b11;
         #10;
-        assert (p == 'h1001) else $fatal("Test case max value failed: %h * %h != %h", a, b, p);
+        assert (p == 'b1001) else $fatal("Test case max value failed: %h * %h != %h", a, b, p);
         
     end
 endmodule
